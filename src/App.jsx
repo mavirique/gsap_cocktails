9a776827f16678940a76ac9bedbaba7dd67f6cfa
mavirique <6375502+mavirique@users.noreply.github.com>
2025-09-01 16:12:59 +0800

    About section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 
 import NavBar from './components/NavBar.jsx';
 import Hero from './components/Hero.jsx';
+import Cocktails from './components/Cocktails.jsx';
+import About from './components/About.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +13,8 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
+      <Cocktails />
+      <About />
     </main>
   );
 };
